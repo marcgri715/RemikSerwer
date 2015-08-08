2d6e38ba0c791ef426b80bd74747d93c16aa35d2
n4zArh <marcgri715@student.polsl.pl>
2015-08-08 14:31:03 +0200

    0.11 - dodanie nowego projektu do solucji (logika gry)

---
+++ RemikGameLogic/Class1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RemikGameLogic
+{
+    public class kupcia
+    {
+        public int kupa;
+        public kupcia() { kupa = 4; }
+    }
+}
